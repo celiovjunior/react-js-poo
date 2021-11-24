ea38f0c4d516a1df30b8a79440716a0cdafbb536
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-11-24 17:15:22 -0300

    polimorfismo e duck typing

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ diretor1.cadastrarSenha("123456789")
 const gerente1 = new Gerente("Arnaldo", 5000, 99966633300)
 gerente1.cadastrarSenha("78899500")
 
-const logado = SistemaAutenticacao.login(gerente1, "78899500")
+const cliente1 = new Cliente("Kdu", 15935788800, 405060)
 
-console.log(logado)
+const gerenteLogado = SistemaAutenticacao.login(gerente1, "78899500")
+const diretorLogado = SistemaAutenticacao.login(diretor1, "123456789")
+const clienteLogado = SistemaAutenticacao.login(cliente1, "405060")
+
+console.log([gerenteLogado, diretorLogado, clienteLogado])
