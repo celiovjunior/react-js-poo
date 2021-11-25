3e3d48e12560f03b68b2da7c0054f846f824329c
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-11-25 01:06:14 -0300

    iniciando a estilizacao da pagina

--- ceep/src/components/NotesList.jsx
+++ ceep/src/components/NotesList.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import NoteCard from './NoteCard';
+import NoteCard from './NoteCard/NoteCard';
 
 class NotesList extends Component {
     render() {
         return(
         <ul>
-          {Array.of("Work", "Work", "Study").map(category => {
+          {Array.of("Work", "Work", "Study").map((category, index) => {
             return(
-              <li>
+              <li key={index}>
                 <h2>{ category }</h2>
                 <NoteCard />
               </li>
