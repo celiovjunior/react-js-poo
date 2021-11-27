4d636ec8f9836565b8a43aa0b036b0e914d1f339
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-11-27 07:20:13 -0300

    refatoracao dos componentes e estilizacao

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,16 +1,17 @@
-import { Component } from 'react';
-import React from 'react';
-import FormRegister from './components/FormRegister';
-import NotesList from './components/NotesList';
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <FormRegister />
-        <NotesList />
-      </div>
-    )
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
   }
 }
 
