a3be0ce68033d38dd93212fbf03faf6db0ae6ad1
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-12-06 23:27:40 -0300

    corrigindo o bug da substituicao do metodo pelo props

--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -8,7 +8,7 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <img src={deleteImg} alt="ícone de deletar" onClick={() => this.props.apagarNota} />
+          <img src={deleteImg} alt="ícone de deletar" onClick={this.props.apagarNota} />
         </header>
 
         <p className="card-nota_texto">{this.props.texto}</p>
