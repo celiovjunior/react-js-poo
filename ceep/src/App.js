72b864ec008240a49325e2978bfc903e2f415413
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-12-14 22:56:46 -0300

    criando o observable na base de dados atraves da criacao de inscritos e do metodo de notificacao

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -10,41 +10,9 @@ import ArrayDeNotas from "./data/Notas";
 class App extends Component {
   constructor() {
     super()
-    // this.state = {
-    //   notas: [],
-    //   //categorias: ['Cursos', 'Trabalho']
-    // }
     this.categorias = new Categorias()
     this.notas = new ArrayDeNotas()
   }
-
-  // criarNota(titulo, texto, categoria) {
-  //   const novaNota = {
-  //     titulo,
-  //     texto,
-  //     categoria
-  //   }
-  //   const novoArrayDeNotas = [...this.state.notas, novaNota]
-  //   const novoEstado = {
-  //     notas: novoArrayDeNotas
-  //   }
-  //   this.setState(novoEstado)
-  // }
-
-  // adicionarCategoria(nomeCategoria) {
-  //   const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
-  //   const novaCategoria = {
-  //     categorias: novoArrayCategorias
-  //   }
-  //   this.setState(novaCategoria)
-  // }
-
-  // deletarNota(index) {
-  //   let arrayDeNotas = this.state.notas
-  //   arrayDeNotas.splice(index, 1) // deletando um item de cada vez
-  //   this.setState({notas: arrayDeNotas})
-  // }
-
 
   render() {
     return (
