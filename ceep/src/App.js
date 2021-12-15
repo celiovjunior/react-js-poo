12c993dd306dae6aa29f39ee113bfdf8bec955b9
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-12-14 22:24:22 -0300

    criando base de dados e refatorando o App com a criacao de novas classes

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -4,56 +4,60 @@ import "./assets/App.css";
 import './assets/index.css';
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 import ListaDeCategorias from "./components/ListaDeCategorias/ListaDeCagegorias";
+import Categorias from "./data/Categorias";
+import ArrayDeNotas from "./data/Notas";
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      notas: [],
-      categorias: ['Cursos', 'Trabalho']
-    }
+    // this.state = {
+    //   notas: [],
+    //   //categorias: ['Cursos', 'Trabalho']
+    // }
+    this.categorias = new Categorias()
+    this.notas = new ArrayDeNotas()
   }
 
-  criarNota(titulo, texto, categoria) {
-    const novaNota = {
-      titulo,
-      texto,
-      categoria
-    }
-    const novoArrayDeNotas = [...this.state.notas, novaNota]
-    const novoEstado = {
-      notas: novoArrayDeNotas
-    }
-    this.setState(novoEstado)
-  }
+  // criarNota(titulo, texto, categoria) {
+  //   const novaNota = {
+  //     titulo,
+  //     texto,
+  //     categoria
+  //   }
+  //   const novoArrayDeNotas = [...this.state.notas, novaNota]
+  //   const novoEstado = {
+  //     notas: novoArrayDeNotas
+  //   }
+  //   this.setState(novoEstado)
+  // }
 
-  adicionarCategoria(nomeCategoria) {
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
-    const novaCategoria = {
-      categorias: novoArrayCategorias
-    }
-    this.setState(novaCategoria)
-  }
+  // adicionarCategoria(nomeCategoria) {
+  //   const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
+  //   const novaCategoria = {
+  //     categorias: novoArrayCategorias
+  //   }
+  //   this.setState(novaCategoria)
+  // }
 
-  deletarNota(index) {
-    let arrayDeNotas = this.state.notas
-    arrayDeNotas.splice(index, 1) // deletando um item de cada vez
-    this.setState({notas: arrayDeNotas})
-  }
+  // deletarNota(index) {
+  //   let arrayDeNotas = this.state.notas
+  //   arrayDeNotas.splice(index, 1) // deletando um item de cada vez
+  //   this.setState({notas: arrayDeNotas})
+  // }
 
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro 
-        criarNota={this.criarNota.bind(this)}
-        categorias={this.state.categorias} 
+        criarNota={this.notas.criarNota}
+        categorias={this.categorias.categorias} 
         />
           <main className="conteudo-principal">
             <ListaDeCategorias 
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias} />
-            <ListaDeNotas apagarNota={this.deletarNota.bind(this)} notas={this.state.notas} />
+            adicionarCategoria={this.categorias.adicionarCategoria}
+            categorias={this.categorias.categorias} />
+            <ListaDeNotas apagarNota={this.notas.apagarNota} notas={this.notas.notas} />
           </main>
       </section>
     );
